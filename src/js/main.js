d84a40c3690ac9ab4d787339668a17895e6902b7
Peter Guntermann <pgu@posteo.de>
2024-07-27 23:43:53 +0200

    make Hühnchen-Slider work

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,14 @@
-import '../scss/index.scss';
+import 'bootstrap';
 
-console.log('$', $);
-console.log('jQuery', jQuery);
-console.log('breakpoints', breakpoints);
-console.log('browser', browser);
+import '../scss/index.scss';
 
 import '../../vendor/html5up-hyperspace/assets/js/jquery.scrollex.min';
 import '../../vendor/html5up-hyperspace/assets/js/jquery.scrolly.min';
 import '../../vendor/html5up-hyperspace/assets/js/main';
 import '../../vendor/html5up-hyperspace/assets/js/util';
 // import '../../vendor/lightbox2-2.11.4/dist/js/lightbox.min.js';
+
+console.log('$', $);
+console.log('jQuery', jQuery);
+console.log('breakpoints', breakpoints);
+console.log('browser', browser);
