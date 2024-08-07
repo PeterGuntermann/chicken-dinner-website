d25aed89abb69b53a5a15487dc67ddb99600201c
Peter Guntermann <pgu@posteo.de>
2024-08-07 21:37:12 +0200

    add huehnchen-slider

--- components/galerie-huehnchen.vue
+++ components/galerie-huehnchen.vue
@@ -1,5 +1,19 @@
 <script setup lang="ts"></script>
 
-<template>Hühnchen-Slider</template>
+<template>
+  <div class="carousel slide carousel-fade" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <NuxtImg src="/images/huehnchen-slider/chicken1.jpg" loading="lazy" />
+      </div>
+      <div class="carousel-item">
+        <NuxtImg src="/images/huehnchen-slider/chicken2.jpg" loading="lazy" />
+      </div>
+      <div class="carousel-item">
+        <NuxtImg src="/images/huehnchen-slider/chicken3.jpg" loading="lazy" />
+      </div>
+    </div>
+  </div>
+</template>
 
 <style scoped></style>
