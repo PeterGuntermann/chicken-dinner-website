609b46b268eb02272079e382b30fcb1611bebe67
Peter Guntermann <pgu@posteo.de>
2024-08-09 21:05:52 +0200

    style links

--- components/footer.vue
+++ components/footer.vue
@@ -9,13 +9,6 @@
 
 <style scoped>
 div > * {
-  color: var(--color-gray-light);
   margin: 0 1.5rem;
-  text-decoration: none;
-  transition: color 0.2s ease;
-
-  &:hover {
-    color: var(--color-yellow);
-  }
 }
 </style>
