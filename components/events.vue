6283bc4d61769607228accbf5a1a9ec4accd5d15
Peter Guntermann <pgu@posteo.de>
2024-08-10 23:50:41 +0200

    feat(events): improve layout and styling

--- components/events.vue
+++ components/events.vue
@@ -3,13 +3,14 @@ type Event = {
   datum: string;
   titel: string;
   link?: string;
+  details?: string;
 };
 
 const content = await queryContent('/events').findOne();
 const events = content.body as unknown as Event[];
 const heute = new Date().getTime();
 const datum = (event: Event) => Date.parse(event.datum);
-const zuletzt = events.filter((event) => datum(event) < heute);
+const zuletzt = events.filter((event) => datum(event) < heute).slice(0, 4);
 const demnaechst = events.filter((event) => datum(event) > heute);
 </script>
 
@@ -19,31 +20,57 @@ const demnaechst = events.filter((event) => datum(event) > heute);
   <div class="mb-5">
     <h3>Demnächst</h3>
 
-    <ul>
-      <li v-for="event in demnaechst">
-        <NuxtTime
-          :datetime="new Date(event.datum)"
-          weekday="short"
-          year="numeric"
-          month="short"
-          day="numeric"
-        />
-        –
-        <a v-if="event.link" :href="event.link" target="_blank"> {{ event.titel }} </a>
-        <span v-else> {{ event.titel }} </span>
-      </li>
-    </ul>
+    <div class="row mb-5">
+      <div class="col-4 offset-4" v-for="event in demnaechst">
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title">
+              <a v-if="event.link" :href="event.link" target="_blank">
+                {{ event.titel }}
+              </a>
+              <span v-else class="color-yellow"> {{ event.titel }} </span>
+            </h4>
+
+            <NuxtTime
+              :datetime="new Date(event.datum)"
+              class="color-gray-dark"
+              weekday="short"
+              year="numeric"
+              month="short"
+              day="numeric"
+            />
+
+            <p v-if="event.details" class="card-text">
+              {{ event.details }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
 
     <h3>Zuletzt</h3>
 
-    <ul>
-      <li v-for="event in zuletzt">
-        <NuxtTime :datetime="new Date(event.datum)" year="numeric" month="short" />
-        –
-        <a v-if="event.link" :href="event.link" target="_blank"> {{ event.titel }} </a>
-        <span v-else> {{ event.titel }} </span>
-      </li>
-    </ul>
+    <div class="row">
+      <div class="col-3" v-for="event in zuletzt">
+        <div class="card">
+          <div class="card-body">
+            <p class="card-title">
+              <a v-if="event.link" :href="event.link" target="_blank">
+                {{ event.titel }}
+              </a>
+              <span v-else> {{ event.titel }} </span>
+            </p>
+
+            <NuxtTime
+              :datetime="new Date(event.datum)"
+              class="color-gray-dark"
+              year="numeric"
+              month="short"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
