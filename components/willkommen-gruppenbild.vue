0195f15d6e03e31fe169e43241d23ce480e43f81
Peter Guntermann <pgu@posteo.de>
2024-08-14 22:58:28 +0200

    add alt attribute to image

--- components/willkommen-gruppenbild.vue
+++ components/willkommen-gruppenbild.vue
@@ -2,7 +2,12 @@
 
 <template>
   <div class="d-flex justify-content-center mb-5">
-    <img class="img-fluid" src="/images/wir/Gruppe-1600w.png" loading="lazy" />
+    <img
+      class="img-fluid"
+      src="/images/wir/Gruppe-1600w.png"
+      loading="lazy"
+      alt="Die sechs Bandmitglieder von Chicken Dinner"
+    />
   </div>
 </template>
 
