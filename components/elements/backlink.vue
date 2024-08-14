888c9d025d11f546443c9baf917695ebf6f41333
Peter Guntermann <pgu@posteo.de>
2024-08-14 19:31:42 +0200

    update Impressum & Datenschutz

--- components/elements/backlink.vue
+++ components/elements/backlink.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <a href="#" onclick="window.history.go(-1); return false;">
-    ← &nbsp;&nbsp;&nbsp; Zurück zur Hauptseite
-  </a>
+  <a href="javascript:history.back()"> ← &nbsp;&nbsp;&nbsp; Zurück zur Hauptseite </a>
 </template>
 
 <style scoped></style>
--- pages/impressum.vue
+++ pages/impressum.vue
@@ -1,9 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Backlink from '~/components/elements/backlink.vue';
+</script>
 
 <template>
-  <h1>impressum page</h1>
+  <div class="text-center my-5">
+    <Backlink />
+  </div>
 
-  <NuxtLink to="/">home</NuxtLink>
+  <div class="container">
+    <h1>Impressum</h1>
+    <h2>Angaben gemäß § 5 TMG</h2>
+    <p>Peter Guntermann<br />Haydnstraße 6<br />76356 Weingarten</p>
+    <h2>Kontakt</h2>
+    <p>Telefon: +49 (0) 162 9804078<br />E-Mail: info@chickendinnerband.de</p>
+    <h2>Redaktionell verantwortlich</h2>
+    <p>Peter Guntermann<br />Haydnstraße 6<br />76356 Weingarten</p>
+    <p>Quelle: <a href="https://www.e-recht24.de">e-recht24.de</a></p>
+  </div>
+
+  <div class="text-center mb-5">
+    <Backlink />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+h1,
+h2,
+h3 {
+  text-align: left;
+}
+
+h2 {
+  margin-top: 3rem;
+}
+</style>
