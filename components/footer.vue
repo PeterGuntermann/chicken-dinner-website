4ae73f8f4b103bdfdee7d19aa34fae35394099cd
Peter Guntermann <pgu@posteo.de>
2024-08-15 17:24:43 +0200

    feat: add link to press kit

--- components/footer.vue
+++ components/footer.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="d-flex justify-content-center p-5">
+  <div class="d-flex justify-content-center flex-wrap p-5">
     <NuxtLink to="/datenschutz">Datenschutz</NuxtLink>
     <NuxtLink to="/impressum">Impressum</NuxtLink>
+    <a href="/presskit-chicken-dinner.zip" title="Press Kit">Press Kit</a>
   </div>
 </template>
 
 <style scoped>
 div > * {
-  margin: 0 1.5rem;
+  margin: 0 1.5rem 2rem;
 }
 </style>
