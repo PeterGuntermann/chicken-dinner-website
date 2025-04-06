d27316b48248469cb61120c72cc80ecaa8fefe7f
Peter Guntermann <pgu@posteo.de>
2025-04-06 10:28:02 +0200

    update npm dependency minor versions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+  },
   app: {},
-  modules: ['@nuxt/content', '@nuxt/fonts', '@nuxt/image', '@nuxt/scripts', 'nuxt-time'],
-  css: ['bootstrap/scss/bootstrap.scss', '~/assets/scss/custom.scss'],
+  modules: [
+    //
+    '@nuxt/content',
+    '@nuxt/fonts',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    'nuxt-time',
+  ],
+  css: [
+    //
+    'bootstrap/scss/bootstrap.scss',
+    '~/assets/scss/custom.scss',
+  ],
 });
