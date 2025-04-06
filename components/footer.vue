d6d96161f7643aec23aa6ad4242cf22d81c8602d
Peter Guntermann <pgu@posteo.de>
2025-04-06 10:33:13 +0200

    provide presskit zip

--- components/footer.vue
+++ components/footer.vue
@@ -4,7 +4,7 @@
   <div class="d-flex justify-content-center flex-wrap p-5">
     <NuxtLink to="/datenschutz">Datenschutz</NuxtLink>
     <NuxtLink to="/impressum">Impressum</NuxtLink>
-    <a href="/presskit-chicken-dinner.zip" title="Press Kit">Press Kit</a>
+    <a href="/presskit/presskit-chicken-dinner.zip" title="Press Kit">Press Kit</a>
   </div>
 </template>
 
